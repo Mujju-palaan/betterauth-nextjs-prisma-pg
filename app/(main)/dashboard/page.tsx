3704bfa87f7798ac7271a7a64ae6a47d50a06faa
Mujju-palaan <mdmujahid97@gmail.com>
2026-02-12 16:54:40 +0530

    all pages upadated + working

--- app/(main)/dashboard/page.tsx
+++ app/(main)/dashboard/page.tsx
@@ -14,7 +14,7 @@ import { format } from "date-fns";
 import { CalendarDaysIcon, MailIcon, ShieldIcon, UserIcon } from "lucide-react";
 import type { Metadata } from "next";
 import Link from "next/link";
-import { unauthorized } from "next/navigation";
+import { unauthorized, redirect  } from "next/navigation";
 
 export const metadata: Metadata = {
   title: "Dashboard",
@@ -24,7 +24,8 @@ export default async function DashboardPage() {
   const session = await getServerSession();
   const user = session?.user;
 
-  if (!user) unauthorized();
+  // if (!user) unauthorized();
+  if (!user) {redirect("/sign-in");}
 
   return (
     <main className="mx-auto w-full max-w-6xl px-4 py-12">
