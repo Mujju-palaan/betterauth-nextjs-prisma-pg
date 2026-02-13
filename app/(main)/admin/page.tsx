76f3eeb3e1647add64f121296b85f1cfc441a80a
Mujju-palaan <mdmujahid97@gmail.com>
2026-02-13 20:36:14 +0530

    auth with google working

--- app/(main)/admin/page.tsx
+++ app/(main)/admin/page.tsx
@@ -1,12 +1,20 @@
+import { getServerSession } from "@/app/lib/get-session";
 import { Skeleton } from "@/components/ui/skeleton";
+import { forbidden, unauthorized } from "next/navigation";
 
-export default function AdminLoading() {
+export default async function AdminLoading() {
+  const session = await getServerSession();
+    const user = session?.user
+
+    if (!user) unauthorized();
+    if (user && user.role !== "admin") forbidden();
+
   return (
-    <main className="mx-auto w-full max-w-6xl px-4 py-12">
+    <div className="mx-auto w-full max-w-6xl px-4 py-12">
       <div className="space-y-3">
         <Skeleton className="h-8 w-20" />
         <Skeleton className="h-5 w-56" />
       </div>
-    </main>
+    </div>
   );
 }
